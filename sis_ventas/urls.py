10e8a6a26e95346ba5249e8a3a13169cbdc2b65e
alarico <alarico@cajalosandes.pe>
2024-02-20 16:19:54 -0500

    url STATIC_URL

--- sis_ventas/urls.py
+++ sis_ventas/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth.views import (LoginView, LogoutView,PasswordChangeView, PasswordResetDoneView)
 from aplicacion.views import inicio, venta, almacen, perfil, guardar_stock, guardar_caja, guardar_venta, eliminar, rptventas, listaventas
 
@@ -21,3 +23,6 @@ urlpatterns = [
     path('rptventas/<str:fecha>/', rptventas, name='rptventas'),
     path('listaventas/', listaventas, name='listaventas'),
 ]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
