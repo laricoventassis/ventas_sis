e76dbfef47f62b444b7d9ab690af1ebab81ad668
alarico <alarico@cajalosandes.pe>
2024-02-20 12:18:28 -0500

    config vercel

--- sis_ventas/settings.py
+++ sis_ventas/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w5$o4k&r6*cr8gea8*)cdftbhrkbt+v)kakf0-l*indu0(-^p8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '.vercel.app']
 
 
 # Application definition
